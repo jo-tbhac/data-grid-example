709bf18b69a2731a12f8f5bff4842b27cb7023f9
junpei ogawa <cornerstone.am3@gmail.com>
2025-03-11 09:46:56 +0900

    setup fontsource

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
 
+import '@fontsource-variable/noto-sans-jp'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />

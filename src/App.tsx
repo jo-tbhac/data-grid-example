f0c74d62ab94f66ebaf6e2f5d87859df60215617
junpei ogawa <cornerstone.am3@gmail.com>
2025-03-12 11:03:55 +0900

    fix warning

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router'
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router'
 import { AgGridPage } from '@/components/AgGrid'
 import { HandsontablePage } from '@/components/Handsontable'
 import { MuiDataGridPage } from '@/components/MuiDataGrid'
@@ -12,7 +12,7 @@ export const App = () => {
           <Route path="/ag-grid" element={<AgGridPage />} />
           <Route path="/handsontable" element={<HandsontablePage />} />
           <Route path="/mui-data-grid" element={<MuiDataGridPage />} />
-          <Route path="/" />
+          <Route path="/" element={<Outlet />} />
         </Route>
       </Routes>
     </BrowserRouter>

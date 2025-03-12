387590ca0218b7a54368d1150c18d6ee1e13fc81
junpei ogawa <cornerstone.am3@gmail.com>
2025-03-12 12:01:42 +0900

    impl AgGrid sample

--- src/components/AgGrid/AgGrid.page.tsx
+++ src/components/AgGrid/AgGrid.page.tsx
@@ -1,6 +1,41 @@
 import { FC } from 'react'
+import { Box, Stack, Typography } from '@mui/material'
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community'
+import { AG_GRID_LOCALE_JP } from '@ag-grid-community/locale'
+import { AgGridReact } from 'ag-grid-react'
+
 import { PageContainer } from '@/components/ui/PageContainer'
+import { rows2, columns2 } from '@/data'
+
+ModuleRegistry.registerModules([AllCommunityModule])
 
 export const AgGridPage: FC = () => {
-  return <PageContainer>AgGridPage</PageContainer>
+  const defaultColDef = { editable: true }
+
+  return (
+    <PageContainer>
+      <Stack direction="row" spacing={1} sx={{ mb: 1 }}>
+        <Typography variant="body2">
+          columns: {columns2.length.toLocaleString('ja-JP')}件
+        </Typography>
+        <Typography variant="body2">rows: {rows2.length.toLocaleString('ja-JP')}件</Typography>
+      </Stack>
+      <Box
+        sx={{
+          flex: 1,
+        }}
+      >
+        <AgGridReact
+          rowData={rows2}
+          columnDefs={columns2}
+          defaultColDef={defaultColDef}
+          localeText={AG_GRID_LOCALE_JP}
+          pagination
+          singleClickEdit
+          undoRedoCellEditing
+          undoRedoCellEditingLimit={20}
+        />
+      </Box>
+    </PageContainer>
+  )
 }
